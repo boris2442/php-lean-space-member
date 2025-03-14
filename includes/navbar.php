5d0fdb56176dc649a79947818194297ca031e8cc
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-14 10:19:11 +0100

    debug

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,7 @@
+
+<?php 
+session_start();
+?>
 <nav>
     <ul>
         <li>Accueil</li>
@@ -6,15 +10,19 @@
 
         <?php if (!isset($_SESSION["user"])) { ?>
             <li><a href="connexion.php">connexion</a></li>
-            <li><a href="connexion.php">Inscription</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
 
-        <?php }
-        ?>
-        <?php
-         else{?>
+        <?php }else{ ?>
+      
+      
+      
 
-            <li><a href="deconnexion.php">Deconnexion</a></li>
-            <?php    }
-        ?>
+
+<li><a href="deconnexion.php">Deconnexion</a></li>
+<?php  }?>
+       
+
+          
+         
     </ul>
 </nav>
