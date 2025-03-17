699e36a75ad65420272fef1bb36f61543ad62b4e
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-17 11:24:39 +0100

    generateur of pdf grace to dompdf

--- genpdf.php
+++ genpdf.php
@@ -1,33 +1,36 @@
 <?php
+
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
 
 
 require_once "database.php";
-$sql="SELECT * FROM `spacemember`";
-$quety=$db->query($sql);
-$users=$query->fetchAll();
+$sql = "SELECT * FROM `spacemember`";
+$query = $db->query($sql);
+$users = $query->fetchAll();
+// var_dump($users);
 
+ob_start();
 require_once "pdf-content.php";
+$html=ob_get_contents();
+// die($html);
+
+
+ob_end_clean();
 
 
 
 
 
 require_once "includes/dompdf-3.1.0/dompdf/autoload.inc.php";
-$options=new Options();
+$options = new Options();
 $options->set('defaultFont', 'Courier');
-$dompdf=new Dompdf();
+$dompdf = new Dompdf($options);
 
-$dompdf->loadHtml('brouete');
+// $dompdf->loadHtml('brouete');
+
+ $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
-$dompdf->stream()
-
-
-
-
-
-
-?>
+$dompdf->stream();
--- pdf-content.php
+++ pdf-content.php
@@ -19,12 +19,12 @@
             <?php
             foreach ($users as $user): ?>
                 <tr>
-                    <td><?php $user['id']?></td>
-                    <td><?php $user['name']?></td>
-                    <td><?php $user['email']?></td>
+                    <td><?php echo $user['id'] ?></td>
+                    <td><?php echo $user['name'] ?></td>
+                    <td><?php echo $user['email'] ?></td>
                 </tr>
-        </tbody>
-    <?php endforeach; ?>
+                <?php endforeach; ?>
+            </tbody>
     </table>
 </body>
 
