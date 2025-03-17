121b6d507d45c312b56c8c6d435d9366b8de9d53
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-17 12:04:48 +0100

    generateur de pdf a l'aide de dompdf

--- genpdf.php
+++ genpdf.php
@@ -3,6 +3,7 @@
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
+require_once "includes/dompdf-3.1.0/dompdf/autoload.inc.php";
 
 
 require_once "database.php";
@@ -13,8 +14,8 @@ $users = $query->fetchAll();
 
 ob_start();
 require_once "pdf-content.php";
-$html=ob_get_contents();
-die($html);
+$html=ob_get_clean();
+// die($html);
 
 
 ob_end_clean();
@@ -23,7 +24,6 @@ ob_end_clean();
 
 
 
-require_once "includes/dompdf-3.1.0/dompdf/autoload.inc.php";
 $options = new Options();
 $options->set('defaultFont', 'Courier');
 $dompdf = new Dompdf($options);
@@ -33,4 +33,4 @@ $dompdf = new Dompdf($options);
  $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
-$dompdf->stream();
+$dompdf->stream("document.pdf", ["Attachment" => false]);
